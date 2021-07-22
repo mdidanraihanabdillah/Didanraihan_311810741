6a6b6993c46a2a63e1b5a73d96faa4c6f049f021
mdidanraihanabdillah <43780985+mdidanraihanabdillah@users.noreply.github.com>
2021-07-22 19:20:53 +0700

    Add files via upload

---
+++ app/src/main/java/activity/project/qrcodedidanraihan/LoginActivity.java
@@ -0,0 +1,26 @@
+package activity.project.qrcodedidanraihan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login = findViewById(R.id.btnlogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/activity/project/qrcodedidanraihan/getStarted.java
@@ -0,0 +1,34 @@
+package activity.project.qrcodedidanraihan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class getStarted extends AppCompatActivity {
+
+    Button start, sign;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+        start = findViewById(R.id.btnstart);
+        sign = findViewById(R.id.btnsign);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getStarted.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getStarted.this, DaftarActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
